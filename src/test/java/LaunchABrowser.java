3ec15f3ec707a60ab5054fcbf9c9a4a586ada56f
Moham <Moham@Mohammed_Arham.home>
2024-10-02 17:47:21 -0400

    First Commit

---
+++ src/test/java/LaunchABrowser.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchABrowser {
+
+	public static void main(String[] args) {
+
+    ChromeDriver driver= new ChromeDriver();
+
+	}
+
+}
