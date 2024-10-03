451019536cc9cdcdcdb00e4698b06246f39732bf
Moham <Moham@Mohammed_Arham.home>
2024-10-03 13:12:05 -0400

    Launch a Chrome browser and open a webpage

--- src/test/java/LaunchABrowser.java
+++ src/test/java/LaunchABrowser.java
@@ -1,11 +1,36 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 
 public class LaunchABrowser {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-    ChromeDriver driver= new ChromeDriver();
-
+		/* Test case
+		 * launch browser
+		 * open url: https://www.amazn.ca
+		 * validate title
+		 * close browser
+		 */
+		
+		//launch browser for chrome
+		
+		WebDriver driver= new ChromeDriver();
+     //ChromeDriver driver= new ChromeDriver();
+       driver.get("https://www.amazon.ca");
+       Thread.sleep(5000);
+     // Validate title  
+       String title= driver.getTitle();
+       if(title.equals("Amazon.ca: Low Prices – Fast Shipping – Millions of Items"))
+       {
+         System.out.println("correct title: "+ title);
+       }else
+       {
+    	 System.out.println("incorrect title"+title);
+       }
+       Thread.sleep(5000);
+       //close browser
+       driver.close();
 	}
 
 }
