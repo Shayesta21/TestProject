f4be7b65caf62a38dc749df5155250939b24aee4
Shayesta <nausheenshayesta21@gmail.com>
2025-02-20 17:44:49 -0500

    Shadow Dom

---
+++ src/test/java/shadowdom/ShadowDomPractice.java
@@ -0,0 +1,30 @@
+package shadowdom;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ShadowDomPractice {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://books-pwakit.appspot.com/");
+		
+		SearchContext shadow =driver.findElement(By.cssSelector("book-app[apptitle='BOOKS']")).getShadowRoot();
+		Thread.sleep(1000);
+		shadow.findElement(By.cssSelector("#input")).sendKeys("Welcome");
+		
+		
+		
+		
+	}
+
+}
